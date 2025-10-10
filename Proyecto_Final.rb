c33993ffab193a72b6433d84c43375bc9d0fee10
Jeanneth Alejandra Moran Alvarez <j.alejandramoran@gmail.com>
2025-10-09 22:36:16 -0600

    Menu - semana1

---
+++ Proyecto_Final.rb
@@ -0,0 +1,9 @@
+puts "===== SISTEMA DE GESTIÓN DE ALUMNOS ====="
+
+puts "1. Registrar estudiante"
+puts "2. Ingresar notas"
+puts "3. Consultar promedio por estudiante"
+puts "4. Listar todos los estudiantes"
+puts "5. Guardar y salir"
+puts "Seleccione una opción:"
+cOpc = gets
