bac55c7a61ce5d787992d34dc452412d7225ce9e
Jeanneth Alejandra Moran Alvarez <j.alejandramoran@gmail.com>
2025-10-10 20:36:05 -0600

    Esqueleto_funciones

--- Proyecto_Final.rb
+++ Proyecto_Final.rb
@@ -1,3 +1,7 @@
+estudiantes = []
+notas = []
+def opcion = gets.chomp
+
 puts "===== SISTEMA DE GESTIÓN DE ALUMNOS ====="
 
 puts "1. Registrar estudiante"
@@ -7,3 +11,27 @@ puts "4. Listar todos los estudiantes"
 puts "5. Guardar y salir"
 puts "Seleccione una opción:"
 cOpc = gets
+
+
+if opcion = 1
+estudiante.times do
+estudiante = gets.chomp.to_f
+estudiantes.push (nombre)
+
+elsif opcion = 2
+nota = gets.chomp.to_i
+notas.push (nota)
+
+elsif opcion = 3
+def promedio (a, b)
+  return a * b / 4
+
+elsif opcion = 4
+puts studiantes.sort
+
+elsif opcion = 5
+save
+
+else
+  puts "Número inválido"
+
